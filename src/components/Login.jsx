c4a179d549700f3bc70d282a6d5e0760954aa308
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-03-31 23:41:06 +0600

    setup basic project

---
+++ src/components/Login.jsx
@@ -0,0 +1,11 @@
+
+
+const Login = () => {
+    return (
+        <div>
+            <h1 className="text-4xl text-blue-600">Log In</h1>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/components/Register.jsx
@@ -0,0 +1,11 @@
+
+
+const Register = () => {
+    return (
+        <div>
+            <h1 className="text-4xl text-green-600">Register</h1>
+        </div>
+    );
+};
+
+export default Register;
